19efbed350e4e2931cc02529e3f3750b2876583d
Dakota Kim <codingvampire@gmail.com>
2021-02-21 13:29:59 -0500

    Initial Commit

---
+++ NoiseTime WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NoiseTime WatchKit Extension
+//
+//  Created by Dakota Kim on 2/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoiseTime WatchKit Extension/NoiseTimeApp.swift
@@ -0,0 +1,19 @@
+//
+//  NoiseTimeApp.swift
+//  NoiseTime WatchKit Extension
+//
+//  Created by Dakota Kim on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct NoiseTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
