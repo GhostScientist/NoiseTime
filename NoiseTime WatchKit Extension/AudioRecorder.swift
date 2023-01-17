4797bc04b427c194bcc31396b83f7b8b75adaf18
Dakota Kim <codingvampire@gmail.com>
2023-01-16 19:30:18 -0500

    functional audio recording & playback

--- NoiseTime WatchKit Extension/AudioRecorder.swift
+++ NoiseTime WatchKit Extension/AudioRecorder.swift
@@ -24,13 +24,48 @@ class AudioRecorder: ObservableObject {
     
     func startRecording() {
         recording = true
+        let recordingSession = AVAudioSession()
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            
+            let fileManager = FileManager.default
+            let documentsUrl = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+            let audioFilename = documentsUrl.first!.appendingPathComponent("recorded_audio.m4a")
+            
+            
+            let settings = [
+                        AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+                        AVSampleRateKey: 12000,
+                        AVNumberOfChannelsKey: 1,
+                        AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+                    ]
+            
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.record()
+            recording = true
+            
+        } catch {
+            print("Failed to set up a recording session.")
+        }
     }
     
     func stopRecording() {
+        audioRecorder.stop()
         recording = false
     }
     
     func fetchRecordings() {
         
+    }
+}
+
+extension Date
+{
+    func toString( dateFormat format  : String ) -> String
+    {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: self)
     }
 }
--- NoiseTime WatchKit Extension/ContentView.swift
+++ NoiseTime WatchKit Extension/ContentView.swift
@@ -7,26 +7,71 @@
 
 import SwiftUI
 import Combine
+import AVFoundation
+
+struct RecordButton: View {
+    
+    @ObservedObject var audioRecorder: AudioRecorder
+    
+    @State var player: AVAudioPlayer!
+    
+    init(audioRecorder: AudioRecorder) {
+        self.audioRecorder = audioRecorder
+    }
+    
+    var body: some View {
+        Button(action: {
+            audioRecorder.recording == true ? audioRecorder.stopRecording() :
+            audioRecorder.startRecording()
+        }) {
+            if (audioRecorder.recording) {
+                Image(systemName: "stop.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 25, height: 25)
+                    .clipped()
+                    .foregroundColor(.red)
+            } else {
+                Image(systemName: "circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 25, height: 25)
+                    .clipped()
+                    .foregroundColor(.red)
+            }
+        }
+        Button {
+            let fileManager = FileManager.default
+            let documentsUrl = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+            let soundFileUrl = documentsUrl.first!.appendingPathComponent("recorded_audio.m4a")
+
+            do {
+                player = try AVAudioPlayer(contentsOf: soundFileUrl)
+                player.prepareToPlay()
+                player.play()
+            } catch {
+                print("Error loading audio file: \(error)")
+            }
+        } label: {
+            Text("Play")
+        }
+
+    }
+    
+}
 
 struct ContentView: View {
     @ObservedObject var audioRecorder: AudioRecorder
     
     var body: some View {
-        VStack {
-            if audioRecorder.recording == false {
-                Button("Start Recording") {
-                    print("Start recording")
-                    audioRecorder.startRecording()
-                }
-            } else {
-                Button("Stop recording") {
-                    print("Stop recording")
-                    audioRecorder.stopRecording()
-                }
+        NavigationView {
+            VStack {
+                RecordButton(audioRecorder: audioRecorder)
             }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
